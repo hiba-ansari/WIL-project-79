54e68bd0fa1b308481ccffe46c9b6d6170d2fce0
Hiba Ansari <hiba.ansari@gmail.com>
2026-09-21 16:01:40 +1000

    docs: modified setup script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,5 @@
 python3 -m venv .venv
 source .venv/bin/activate
 
-# install Ollama
-
-
-# install Ollama dependencies
-ollama pull nomic-embed-text
-
 # install dependencies
 pip install -r requirements.txt
